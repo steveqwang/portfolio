771062356cd090b0a8661b386e7bb6f76001255e
stevewang <596899832@qq.com>
2020-04-21 23:58:42 -0400

    almost done

--- portfolio-site/src/App.js
+++ portfolio-site/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Link } from "react-router-dom"
 import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import './App.css';
+
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
 
 class App extends React.Component {
 
@@ -18,9 +22,9 @@ class App extends React.Component {
         {title: 'Contact', path: '/contact' }
       ],
       home: {
-        title: 'BeRelentless',
-        subTitle: 'Projects the make a difference',
-        text: 'Checkout my projects below'
+        title: '王宇',
+        subTitle: 'jjjjjjjjjjjjj',
+        text: 'bbbbbbbbbbb'
       },
       about: {
         title: 'About Me'
@@ -36,21 +40,24 @@ class App extends React.Component {
       <Router>
         <Container className="p-0" fluid={true}>
 
-          <Navbar className="border-bottom">
-            <Navbar.Brand>Steve Love</Navbar.Brand>
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>Steve Wang</Navbar.Brand>
 
+            <Navbar.Toggle area-control="navbar-toggle"/>
             <Navbar.Collapse aria-controls="navbar-toggle">
-              <Nav>
+              <Nav className="ml-auto">
                 <Link className="nav-link" to="/">Home</Link>
-                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/about">About</Link>  
                 <Link className="nav-link" to="/contact">Contact</Link>
               </Nav>
             </Navbar.Collapse>
-
-
-
           </Navbar>
 
+          <Route path="/" exact render={() =>  <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} /> } /> 
+          <Route path="/about" render={() =>  <AboutPage title={this.state.about.title} /> } /> 
+          <Route path="/contact" render={() =>  <ContactPage title={this.state.contact.title} /> } /> 
+
+          <Footer />
 
         </Container>
       </Router>
---
+++ portfolio-site/src/components/Carousel.js
@@ -0,0 +1,83 @@
+import React from 'react';
+
+import Card from '../components/Card';
+
+import cv from '../assets/images/icon_cv.png';
+import intern from '../assets/images/icon_intern.png';
+import code from '../assets/images/icon_code.png';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
+class Carousel extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [
+                {
+                    id: 0,
+                    title: 'Dev Grub',
+                    subTitle: 'The cookbook for developers',
+                    imgSrc: cv,
+                    link: 'https://github.com/steveqwang',
+                    selected: false
+                },
+                {
+                    id: 1,
+                    title: 'Garrett Love',
+                    subTitle: 'YouTube channel',
+                    imgSrc: intern,
+                    link: 'https://github.com/steveqwang',
+                    selected: false
+                },
+                {
+                    id: 2,
+                    title: 'Evverest',
+                    subTitle: 'A social network for developers',
+                    imgSrc: code,
+                    link: 'https://github.com/steveqwang',
+                    selected: false
+                },
+            ]
+        }
+    }
+
+    handleCardClick = (id, card) => {
+
+        let items = [...this.state.items];
+
+        items[id].selected = items[id].selected ? false : true;
+
+        items.forEach(item => {
+            if(item.id !== id) {
+                item.selected = false;
+            }
+        });
+
+        this.setState({
+            items
+        });
+    }
+
+
+    makeItems = (items) => {
+        return items.map(item => {
+            return <Card item={item} click={(e => this.handleCardClick(item.id, e))} key={item.id} />
+        })
+    }
+
+
+    render() {
+        return(
+            <Container fluid={true}>
+                <Row className="justify-content-around">
+                    {this.makeItems(this.state.items)}
+                </Row>
+            </Container>
+        );
+    }
+
+}
+
+export default Carousel;
